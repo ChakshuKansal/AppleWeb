96c2fc326690e087c8f9addc89dd76f05cabb574
Chakshu Kansal <122335747+ChakshuKansal@users.noreply.github.com>
2024-02-25 19:26:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,308 @@
+
+   function showstore(){
+        var x=document.getElementById('dropdownmenu');
+        x.style.top="45px";
+        var content=document.getElementById('store');
+        content.style.opacity=1;
+   
+        var a=document.getElementById('Mac');
+        var b=document.getElementById('iPad');
+        var c=document.getElementById('Mac');
+        var d=document.getElementById('Mac');
+        var e=document.getElementById('Mac');
+        var f=document.getElementById('Mac');
+        var g=document.getElementById('Mac');
+        var h=document.getElementById('Mac');
+        var i=document.getElementById('Mac');
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+    }
+
+    function shownext(){
+        var bar=document.getElementById('displaybox');
+    
+        bar.style.opacity=0;
+    }
+    function showmac(){
+        var x=document.getElementById('dropdownmenu');
+        x.style.top="45px";
+        var content=document.getElementById('Mac');
+        content.style.opacity=1;
+        x.style.height=content.style.height;
+        
+        var a=document.getElementById('store');
+        var b=document.getElementById('iPad');
+        var c=document.getElementById('iPhone');
+        var d=document.getElementById('Watch');
+        var e=document.getElementById('air');
+        var f=document.getElementById('tv');
+        var g=document.getElementById('enter');
+        var h=document.getElementById('access');
+        var i=document.getElementById('Sup');
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+    }
+    function showpad(){
+        var x=document.getElementById('dropdownmenu');
+        x.style.top="45px";
+        var content=document.getElementById('iPad');
+        content.style.opacity=1;
+     
+        
+        var a=document.getElementById('store');
+        var b=document.getElementById('Mac');
+        var c=document.getElementById('iPhone');
+        var d=document.getElementById('Watch');
+        var e=document.getElementById('air');
+        var f=document.getElementById('tv');
+        var g=document.getElementById('enter');
+        var h=document.getElementById('access');
+        var i=document.getElementById('Sup');
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+    }
+    function showphone(){
+        var x=document.getElementById('dropdownmenu');
+        x.style.top="45px";
+        var content=document.getElementById('iPhone');
+        content.style.opacity=1;
+     
+        
+        var a=document.getElementById('store');
+        var b=document.getElementById('Mac');
+        var c=document.getElementById('iPad');
+        var d=document.getElementById('Watch');
+        var e=document.getElementById('air');
+        var f=document.getElementById('tv');
+        var g=document.getElementById('enter');
+        var h=document.getElementById('access');
+        var i=document.getElementById('Sup');
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+    }
+    function showwatch(){
+        var x=document.getElementById('dropdownmenu');
+        x.style.top="45px";
+        var content=document.getElementById('Watch');
+        content.style.opacity=1;
+     
+        var a=document.getElementById('store');
+        var b=document.getElementById('Mac');
+        var c=document.getElementById('iPad');
+        var d=document.getElementById('iPhone');
+        var e=document.getElementById('air');
+        var f=document.getElementById('tv');
+        var g=document.getElementById('enter');
+        var h=document.getElementById('access');
+        var i=document.getElementById('Sup');
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+    }
+    function showpods(){
+        var x=document.getElementById('dropdownmenu');
+        x.style.top="45px";
+        var content=document.getElementById('air');
+        content.style.opacity=1;
+     
+        var a=document.getElementById('store');
+        var b=document.getElementById('Mac');
+        var c=document.getElementById('iPad');
+        var d=document.getElementById('iPhone');
+        var e=document.getElementById('Watch');
+        var f=document.getElementById('tv');
+        var g=document.getElementById('enter');
+        var h=document.getElementById('access');
+        var i=document.getElementById('Sup');
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+    }
+    function showtv(){
+        var x=document.getElementById('dropdownmenu');
+        x.style.top="45px";
+        var content=document.getElementById('tv');
+        content.style.opacity=1;
+     
+        var a=document.getElementById('store');
+        var b=document.getElementById('Mac');
+        var c=document.getElementById('iPad');
+        var d=document.getElementById('iPhone');
+        var e=document.getElementById('Watch');
+        var f=document.getElementById('air');
+        var g=document.getElementById('enter');
+        var h=document.getElementById('access');
+        var i=document.getElementById('Sup');
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+    }
+    function showenter(){
+        var x=document.getElementById('dropdownmenu');
+        x.style.top="45px";
+        var content=document.getElementById('enter');
+        content.style.opacity=1;
+     
+        var a=document.getElementById('store');
+        var b=document.getElementById('Mac');
+        var c=document.getElementById('iPad');
+        var d=document.getElementById('iPhone');
+        var e=document.getElementById('Watch');
+        var f=document.getElementById('air');
+        var g=document.getElementById('tv');
+        var h=document.getElementById('access');
+        var i=document.getElementById('Sup');
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+    }
+    function showaccess(){
+        var x=document.getElementById('dropdownmenu');
+        x.style.top="45px";
+        var content=document.getElementById('access');
+        content.style.opacity=1;
+     
+        var a=document.getElementById('store');
+        var b=document.getElementById('Mac');
+        var c=document.getElementById('iPad');
+        var d=document.getElementById('iPhone');
+        var e=document.getElementById('Watch');
+        var f=document.getElementById('air');
+        var g=document.getElementById('tv');
+        var h=document.getElementById('enter');
+        var i=document.getElementById('Sup');
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+    }
+    function showsup(){
+        var x=document.getElementById('dropdownmenu');
+        x.style.top="45px";
+        var content=document.getElementById('Sup');
+        content.style.opacity=1;
+     
+        var a=document.getElementById('store');
+        var b=document.getElementById('Mac');
+        var c=document.getElementById('iPad');
+        var d=document.getElementById('iPhone');
+        var e=document.getElementById('Watch');
+        var f=document.getElementById('air');
+        var g=document.getElementById('tv');
+        var h=document.getElementById('access');
+        var i=document.getElementById('enter');
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+    }
+
+    function show(){
+        var x=getElementById('displaybox');
+        x.style.top="45px";
+    }
+    function normal(){
+        var x=document.getElementById('displaybox');
+        var y=document.getElementById('dropdownmenu');
+        if(x.style.top=="-120%"){
+            x.style.top="45px";
+        }
+        else{
+            x.style.top="-120%";
+        }
+
+
+        if(y.style.top==="-120%"){
+            y.style.top="45px";
+            x.style.opacity=1;
+        }
+        else{
+            y.style.top="-120%";
+
+            
+            var a=document.getElementById('store');
+            var b=document.getElementById('iPad');
+        var c=document.getElementById('iPhone');
+        var d=document.getElementById('Watch');
+        var e=document.getElementById('air');
+        var f=document.getElementById('tv');
+        var g=document.getElementById('enter');
+        var h=document.getElementById('access');
+        var i=document.getElementById('Sup');
+        var j=document.getElementById('Mac');
+        
+        a.style.opacity=0;
+        b.style.opacity=0;
+        c.style.opacity=0;
+        d.style.opacity=0;
+        e.style.opacity=0;
+        f.style.opacity=0;
+        g.style.opacity=0;
+        h.style.opacity=0;
+        i.style.opacity=0;
+        j.style.opacity=0;
+    }
+}
+
